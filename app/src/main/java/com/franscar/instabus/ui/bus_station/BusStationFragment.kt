5ae9d6780b5347ab546e259debe62a7da08a492d
Oscar Di Lenarda <oscar@dilenarda.com>
2021-01-14 17:39:30 +0100

    Added word-wrap to card titles and uses correct details fragment titles

--- app/src/main/java/com/franscar/instabus/ui/bus_station/BusStationFragment.kt
+++ app/src/main/java/com/franscar/instabus/ui/bus_station/BusStationFragment.kt
@@ -1,18 +1,15 @@
 package com.franscar.instabus.ui.bus_station
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import com.franscar.instabus.MainActivity
 import com.franscar.instabus.R
 import com.franscar.instabus.databinding.FragmentBusStationBinding
 import com.franscar.instabus.ui.shared.SharedViewModel
@@ -26,6 +23,8 @@ class BusStationFragment : Fragment() {
 
         navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment)
         busStationViewModel = ViewModelProvider(requireActivity()).get(SharedViewModel::class.java)
+
+        (activity as MainActivity).supportActionBar?.title = busStationViewModel.selectedBusStation.value?.street_name
 
         val binding = FragmentBusStationBinding.inflate(inflater, container, false)
         binding.busStationViewModel = busStationViewModel
