e70d894a37ef5224f12342afbffb6c4ee7cf2c32
Francois Leclercq <francois.leclercq@supinfo.com>
2021-01-22 19:58:42 +0100

    Buggy animations

--- app/src/main/java/com/franscar/instabus/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/franscar/instabus/ui/settings/SettingsFragment.kt
@@ -1,32 +1,63 @@
 package com.franscar.instabus.ui.settings
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.annotation.ColorInt
+import androidx.core.content.ContextCompat
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
 import com.franscar.instabus.R
+import dev.sasikanth.colorsheet.ColorSheet
+import dev.sasikanth.colorsheet.utils.ColorSheetUtils
+import kotlinx.android.synthetic.main.home_list_item.*
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
-    private lateinit var settingsViewModel: SettingsViewModel
-
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        settingsViewModel = ViewModelProvider(this).get(SettingsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_settings, container, false)
-
-        val textView: TextView = root.findViewById(R.id.text_settings)
-        settingsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-
-        return root
+    companion object {
+        private const val COLOR_SELECTED = "selectedColor"
+        private const val NO_COLOR_OPTION = "noColorOption"
     }
+
+    private var selectedColor: Int = ColorSheet.NO_COLOR
+    private var noColorOption = false
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val colors = resources.getIntArray(R.array.colors)
+        selectedColor = savedInstanceState?.getInt(COLOR_SELECTED) ?: colors.first()
+        //setColor(selectedColor)
+
+        noColorOption = savedInstanceState?.getBoolean(NO_COLOR_OPTION) ?: false
+
+        val myPref: Preference? = findPreference("cardColorDark") as Preference?
+        myPref?.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            ColorSheet()
+                .colorPicker(
+                    colors = resources.getIntArray(R.array.colors),
+                    noColorOption = false,
+                    selectedColor = savedInstanceState?.getInt("selectedColor")
+                        ?: resources.getIntArray(R.array.colors).first(),
+                    listener = { color ->
+                        selectedColor = color
+                        //setColor(selectedColor)
+                    })
+                .show(parentFragmentManager)
+            true
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(COLOR_SELECTED, selectedColor)
+        outState.putBoolean(NO_COLOR_OPTION, noColorOption)
+    }
+
+    /*private fun setColor(@ColorInt color: Int) {
+        home_card.setBackgroundColor(color)
+    }*/
 }
